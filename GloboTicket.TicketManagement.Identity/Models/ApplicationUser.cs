9590980c8e85c191be36dec0d8aaf3d0e53d620c
VenkataSiva Reddy Padala <siva@logics.com>
2024-02-19 10:53:17 +0530

    Demo Branch Created

--- GloboTicket.TicketManagement.Identity/Models/ApplicationUser.cs
+++ GloboTicket.TicketManagement.Identity/Models/ApplicationUser.cs
@@ -4,6 +4,9 @@ namespace GloboTicket.TicketManagement.Identity.Models
 {
     public class ApplicationUser : IdentityUser
     {
-  
+        public int MyProperty { get; set; }
+
+        public int MyProperty1 { get; set; }
+        public int MyProperty2 { get; set;}
     }
 }
